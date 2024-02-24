233352b163b061b3c887a8965de59b8b387686b9
CONSTRUAMBIENTE <jhongutierrezm128@gmail.com>
2024-02-24 16:12:55 -0600

    fourth commit

--- app/src/main/java/com/jhongu/explorerlayouts/ConstraintActivity.java
+++ app/src/main/java/com/jhongu/explorerlayouts/ConstraintActivity.java
@@ -2,7 +2,9 @@ package com.jhongu.explorerlayouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ConstraintActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ConstraintActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint);
+    }
+
+    public void regresarMenu(View vista){
+        Intent miIntent = new Intent(this, MainActivity.class);
+        startActivity(miIntent);
     }
 }
--- app/src/main/java/com/jhongu/explorerlayouts/TableActivity.java
+++ app/src/main/java/com/jhongu/explorerlayouts/TableActivity.java
@@ -2,7 +2,9 @@ package com.jhongu.explorerlayouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TableActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class TableActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table);
+    }
+
+    public void regresarMenu2(View vista){
+        Intent miIntent = new Intent(this, MainActivity.class);
+        startActivity(miIntent);
     }
 }
